ac6cc5ef6c76ea59615a423ce9856ff0176d3f9d
Daniel Hellfeld <dhellfeld@berkeley.edu>
2017-06-09 10:12:27 -0700

    Move cmake3 to cmake

--- PRISM_Sim/PRISM_Sim/compile.sh
+++ PRISM_Sim/PRISM_Sim/compile.sh
@@ -2,6 +2,6 @@ echo "\nRemoving old build"
 rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt PRISM Makefile
 
 echo "\nBuilding...\n"
-cmake3 -DGeant4_DIR=/Users/Hellfeld/GEANT4.10.2.p1/geant4-install/lib/Geant4-10.2.1/ .
+cmake -DGeant4_DIR=/Users/Hellfeld/GEANT4.10.2.p1/geant4-install/lib/Geant4-10.2.1/ .
 
 echo "\nDone! Now run $ make to compile.\n"
