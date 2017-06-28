75f783f85a39f766f5a2789eba2659fbb82adff7
dmraji <davidmraji@gmail.com>
2017-06-28 10:42:31 -0700

    ease of access

--- PRISM_Sim/PRISM_Sim_MT/compile.sh
+++
@@ -1,6 +0,0 @@
-echo "Removing old build"
-rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt PRISM_MT Makefile
-
-echo "Building..."
-cmake3 -DGeant4_DIR=/Users/Hellfeld/GEANT4.10.2.p1_MT/geant4-install/lib/Geant4-10.2.1/ .
-
